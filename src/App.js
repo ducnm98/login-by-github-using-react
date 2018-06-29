e201a912a47388a81ba71777f633615d7db6e547
John Michael <ducnm.john98@gmail.com>
2018-06-29 16:44:28 +0700

    Overwrite App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,82 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const CLIENT_ID = '8f920792d996ef2a2927';
+const REDIRECT_URI = 'http://localhost:3000';
+const CLIENT_SECRET = '87a68f85b5d75648a0ff4c7b9c439f8b34777829';
 
 class App extends Component {
+  state = {
+    status: 'INITIAL',
+    token: null,
+    code: null,
+    avatar_url: null,
+    name: null,
+  }
+
+  componentDidMount() {
+    const code =
+      window.location.href.match(/\?code=(.*)/) &&
+      window.location.href.match(/\?code=(.*)/)[1];
+
+      if (code) {
+
+      this.setState({
+        code: code,
+        status: 'FINISHED_LOADING',
+      });
+
+      fetch('https://github.com/login/oauth/access_token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*' 
+        },
+        body: JSON.stringify({
+          client_id: `${CLIENT_ID}`,
+          code: code,
+          redirect_uri: `${REDIRECT_URI}`,
+          client_secret: `${CLIENT_SECRET}`,
+        })
+      }).then(res => res.json())
+      .then(access_token => {
+        console.log(access_token);
+        this.setState({
+          token: access_token,
+        })
+      })
+
+      // let token = getState(token => { return token; });
+      // if (token) {
+      //   fetch(`https://api.github.com/user?access_token=${token}`)
+      //   .then(res => res.json())
+      //   .then((userInfo) => {
+      //     this.setState({
+      //       avatar_url: userInfo.avatar_url,
+      //       name: userInfo.name, 
+      //     })
+
+      //     getState((avatar_url, name) => {
+      //       let info = (
+      //         <div>
+      //           <img src={avatar_url} />
+      //           <h1>{name}</h1>
+      //         </div>
+      //       );
+      //     })
+      //   })
+      // }
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <a href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`}>
+          Login
+        </a>
+        {/* {info} */}
       </div>
+        
     );
   }
 }
